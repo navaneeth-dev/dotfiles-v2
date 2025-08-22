a9469c644c712746243fc3e7b6f1fd00e2b54a12
Navaneeth <32010211+navaneeth-dev@users.noreply.github.com>
2025-08-22 19:08:23 +0530

    feat: add instaloader pkg

--- home/run_once_install_ansible.sh
+++ home/run_once_install_ansible.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 sudo pacman -S --noconfirm python-pipx
 pipx ensurepath
 sudo pipx ensurepath --global
@@ -8,3 +8,4 @@ sudo pipx ensurepath --global
 pipx install --include-deps ansible ansible-dev-tools
 pipx install --include-deps frida-tools
 
+pipx install --include-deps instaloader
